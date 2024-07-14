bc01153fc9b7b569de1ae14e358599e065eb976e
Predrag Lazic <predrag.lazic@hotmail.com>
2024-07-14 17:27:55 -0400

    cc

--- public/en.search.min.874a83b829bcab9efe1145b1ef933e25fa17777c6595d9f40abc8d7d3951a211.js
+++ public/en.search.min.874a83b829bcab9efe1145b1ef933e25fa17777c6595d9f40abc8d7d3951a211.js
@@ -1,4 +1,4 @@
-'use strict';(function(){const searchDataURL='/en.search-data.min.3363109d43e0c066f412624c7a99cf064990dab5fce34720953a0822d5a7f055.json';const indexConfig=Object.assign({cache:true},{doc:{id:'id',field:['title','content'],store:['title','href','section']}});const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');if(!input){return}
+'use strict';(function(){const searchDataURL='/en.search-data.min.9de579232d8e4c013dd939e0e2bc59cfaee536a5459b7ad58ecf198e33734c90.json';const indexConfig=Object.assign({cache:true},{doc:{id:'id',field:['title','content'],store:['title','href','section']}});const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');if(!input){return}
 input.addEventListener('focus',init);input.addEventListener('keyup',search);document.addEventListener('keypress',focusSearchFieldOnKeyPress);function focusSearchFieldOnKeyPress(event){if(input===document.activeElement){return;}
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
